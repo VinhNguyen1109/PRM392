435c0d08e806186e40b9e4f7434b8383a550a6d5
vuquanghung247 <hung.vuquang@vti.com.vn>
2025-07-11 12:36:27 +0700

    add notification schedule

--- app/src/main/java/com/example/familynoteapp/feture/schedule/ScheduleViewModel.java
+++ app/src/main/java/com/example/familynoteapp/feture/schedule/ScheduleViewModel.java
@@ -1,6 +1,7 @@
 package com.example.familynoteapp.feture.schedule;
 
 import android.app.Application;
+import android.content.Context;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
@@ -9,6 +10,7 @@ import androidx.lifecycle.LiveData;
 import com.example.familynoteapp.dao.ScheduleDao;
 import com.example.familynoteapp.db.AppDatabaseSingleton;
 import com.example.familynoteapp.model.ScheduleTask;
+import com.example.familynoteapp.util.TaskScheduler;
 
 import java.util.List;
 import java.util.concurrent.Executor;
@@ -28,7 +30,16 @@ public class ScheduleViewModel extends AndroidViewModel {
     }
 
     public void insert(ScheduleTask task) {
-        executor.execute(() -> scheduleDao.insert(task));
+        executor.execute(() -> scheduleDao.insertAndReturnId(task));
+    }
+
+    public void insertAndSchedule(ScheduleTask task, Context context) {
+        executor.execute(() -> {
+            // Lấy ID trả về sau khi insert
+            long id = AppDatabaseSingleton.getInstance(context).scheduleDao().insertAndReturnId(task);
+            task.setId((int) id);
+            TaskScheduler.scheduleTaskNotification(context, task);
+        });
     }
 
     public void update(ScheduleTask task) {
@@ -40,9 +51,6 @@ public class ScheduleViewModel extends AndroidViewModel {
     }
 
     public LiveData<List<ScheduleTask>> getAllSchedules() {
-        return AppDatabaseSingleton.getInstance(getApplication())
-                .scheduleDao()
-                .getAllSchedules();  // đảm bảo ScheduleDao có hàm này
+        return scheduleDao.getAllSchedules();
     }
-
 }
